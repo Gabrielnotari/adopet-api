5b5b266a80ebda64e892f3f8d3ef2a04fa7b8907
gabriel <gabriel.notari@icloud.com>
2025-06-22 20:01:22 -0300

    primeiro commit

---
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamento.java
@@ -0,0 +1,24 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ValidacaoPetComAdocaoEmAndamento implements ValidacaoSolicitacaoAdocao{
+
+    @Autowired
+    AdocaoRepository adocaoRepository;
+
+    public void validar(SolicitacaoAdocaoDTO dto){
+        boolean petTemAdocaoEmAndamento = adocaoRepository
+                .existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO);
+
+        if (petTemAdocaoEmAndamento) {
+            throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
+        }
+    }
+}
